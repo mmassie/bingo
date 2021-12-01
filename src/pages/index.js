db4ce8da826b2fc09db8fefafed647ecdd56994d
Mike Massie <mike@michaelmassie.com>
2021-12-01 08:26:37 -0600

    2nd

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import App from "../components/app";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Clutter score your junk drawer</h1>
+    <h1>Bingo Sheet</h1>
     <p>Tally up you score and let's hear it:</p>
     <App />
   </Layout>
